2ff87ff087d63f5c8ff06f5a9396bf5eb6d0a9aa
Sabbir Mahmud Afridi <smahmuddz@gmail.com>
2022-12-03 00:58:49 +0600

    Update Day 1.py

--- Day 1.py
+++ Day 1.py
@@ -1,15 +1,4 @@
-f = open("solution.txt", "r")
-sum=0;
-load=[]
-
-for line in f:
-  if (line.strip()):
-    sum+=int(line)
-  else:
-      load.append(sum)
-      sum=0
-print(max(load))      
-
-load.sort(reverse=True)
-print(load[0]+load[1]+load[2])
-  
+import sys
+load=sorted([sum([int(n) for n in l.split('\n')]) for l in open("input.txt").read().split('\n\n')])
+print(max(load))
+print(sum(load[-3:]))
