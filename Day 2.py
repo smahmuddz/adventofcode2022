ccb13d3c6d0488725c215d4a8cf42bd585def345
Sabbir Mahmud Afridi <smahmuddz@gmail.com>
2022-12-03 18:52:09 +0600

    Delete Day 2.py

--- Day 2.py
+++
@@ -1,6 +0,0 @@
-r=["".join(l.strip().split()) for l in open("solution.txt","r")]
-r1 = {"AX":4,"AY":8,"AZ":3,"BX":1,"BY":5,"BZ":9,"CX":7,"CY":2,"CZ":6}
-r2 = {"AX":3,"AY":4,"AZ":8,"BX":1,"BY":5,"BZ":9,"CX":2,"CY":6,"CZ":7}
-print(sum([r1[i] for i in r]))
-print(sum([r2[i] for i in r]))
-
